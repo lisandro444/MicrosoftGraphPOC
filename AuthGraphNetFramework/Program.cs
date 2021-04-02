248443214f8a90d2fd599cdab659d0c0ee9d1a8b
Lisandro Rossi <Lisandrorossi444@gmail.com>
2021-04-02 14:00:29 -0300

    Initial commit

---
+++ AuthGraphNetFramework/Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthGraphNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                getUsersAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        public async static Task getUsersAsync()
+        {
+            var clientId = "3d05fbdd-713c-40d7-be36-3b2a7344d860";
+            var tenantId = "629fd4e8-9d26-4da5-85ff-cc01ca1948c4";
+            var clientSecret = "C-vI8s0VlB1TCTY~lq39y1dg5Q~tZ9kxX.";
+
+
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithTenantId(tenantId)
+                .WithClientSecret(clientSecret)
+                .Build();
+
+            ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var groups = await graphClient.Groups.Request().Select(x => new { x.Id, x.DisplayName }).GetAsync();
+
+            var message = new Message
+            {
+                Subject = "Testing Microsfot Graph",
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Html,
+                    Content = "<h3>Test works</h3>"
+                },
+                ToRecipients = new List<Recipient>()
+    {
+        new Recipient
+        {
+            EmailAddress = new EmailAddress
+            {
+                Address = "lisandrorossi444@gmail.com"
+            }
+        }
+    },
+                CcRecipients = new List<Recipient>()
+    {
+        new Recipient
+        {
+            EmailAddress = new EmailAddress
+            {
+                Address = "admin@lisandrorossi444.onmicrosoft.com"
+            }
+        }
+    }
+            };
+
+            var saveToSentItems = false;
+
+            await graphClient.Users["admin@lisandrorossi444.onmicrosoft.com"]
+                    .SendMail(message, saveToSentItems)
+                    .Request()
+                    .PostAsync();
+
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.DisplayName}, {group.Id}");
+            }
+        }
+    }
+}
---
+++ graph-tutorial/Helpers/GraphHelper.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Graph;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using graph_tutorial.TokenStorage;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace graph_tutorial.Helpers
+{
+    public static class GraphHelper
+    {
+        // Load configuration settings from PrivateSettings.config
+        private static string appId = ConfigurationManager.AppSettings["ida:AppId"];
+        private static string appSecret = ConfigurationManager.AppSettings["ida:AppSecret"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+        private static List<string> graphScopes = new List<string>(ConfigurationManager.AppSettings["ida:AppScopes"].Split(' '));
+
+        public static async Task<CachedUser> GetUserDetailsAsync(string accessToken)
+        {
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", accessToken);
+                        return Task.FromResult(0);
+                    }));
+
+            var user = await graphClient.Me.Request()
+                .Select(u => new
+                {
+                    u.DisplayName,
+                    u.Mail,
+                    u.UserPrincipalName
+                })
+                .GetAsync();
+
+            return new CachedUser
+            {
+                Avatar = string.Empty,
+                DisplayName = user.DisplayName,
+                Email = string.IsNullOrEmpty(user.Mail) ?
+                    user.UserPrincipalName : user.Mail
+            };
+        }
+
+        public static async Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            var graphClient = GetAuthenticatedClient();
+
+            var events = await graphClient.Me.Events.Request()
+                .Select("subject,organizer,start,end")
+                .OrderBy("createdDateTime DESC")
+                .GetAsync();
+
+            var message = new Message
+            {
+                Subject = "testing de mail",
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Text,
+                    Content = "The new cafeteria is open."
+                },
+                ToRecipients = new List<Recipient>()
+    {
+        new Recipient
+        {
+            EmailAddress = new EmailAddress
+            {
+                Address = "lisandrorossi444@gmail.com"
+            }
+        }
+    },
+                CcRecipients = new List<Recipient>()
+    {
+        new Recipient
+        {
+            EmailAddress = new EmailAddress
+            {
+                Address = "admin@lisandrorossi444.onmicrosoft.com"
+            }
+        }
+    }
+            };
+
+            var saveToSentItems = false;
+
+            await graphClient.Me
+                .SendMail(message, saveToSentItems)
+                .Request()
+                .PostAsync();
+
+            return events.CurrentPage;
+        }
+
+        private static GraphServiceClient GetAuthenticatedClient()
+        {
+            return new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        var idClient = ConfidentialClientApplicationBuilder.Create(appId)
+                            .WithRedirectUri(redirectUri)
+                            .WithClientSecret(appSecret)
+                            .Build();
+
+                        var tokenStore = new SessionTokenStore(idClient.UserTokenCache,
+                            HttpContext.Current, ClaimsPrincipal.Current);
+
+                        var accounts = await idClient.GetAccountsAsync();
+
+                        // By calling this here, the token can be refreshed
+                        // if it's expired right before the Graph call is made
+                        var result = await idClient.AcquireTokenSilent(graphScopes, accounts.FirstOrDefault())
+                            .ExecuteAsync();
+
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                    }));
+        }
+    }
+}
